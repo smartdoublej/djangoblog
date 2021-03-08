3ea59797a4e6824b0beb9222d8ebef4f9e39d06b
smartdoublej <1535086745@qq.com>
2021-03-08 16:49:33 +0800

    数据库配置

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -74,11 +74,23 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+} 
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djblog',# 数据库名
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'POST': '3306',
+    },
 }
 
 
